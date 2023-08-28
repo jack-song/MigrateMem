7569a16d2d72643f4f937496d114015d4abd08a7
marcello.balduccini@gmail.com <marcello.balduccini@gmail.com>
2023-08-28 16:57:20 -0400

    Download link feature added. Touch command support added.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Converts the output of a Mem.ai export to standard markdown files, such as for o
 - Tags should just work out of the box.
 - Links will be correctly converted to local links where possible in the standard `[text](file.md)` format.
   - Not the `[[note]]` format since this is a less surprising way to preserve the `text` and handles hairy corner cases like nesting.
+- Attachments are downloaded locally and their links converted to local links
 
 ## Usage
 
@@ -13,7 +14,7 @@ Converts the output of a Mem.ai export to standard markdown files, such as for o
 2. Create a subfolder in vault.
 3. Move the JSON file to the sub folder.
 4. Move `import_mem_json.py` to the subfolder.
-5. Double check `MEM_JSON_PATH`.
+5. Double check `MEM_JSON_PATH`, `ATTACHMENT_FOLDER_PATH`, `TOUCH_COMMAND_AVAILABLE`, and `SETFILE_COMMAND_AVAILABLE`.
 6. `pip install python-dateutil` or env equivalent.
 7. `python import_mem_json.py`
 8. Done!
--- import_mem_json.py
+++ import_mem_json.py
@@ -5,11 +5,18 @@ import os
 from dateutil import parser
 from subprocess import call
 from urllib.parse import quote
+# for downloading attachments
+import requests
 
 MEM_JSON_PATH = 'mems.json'
+ATTACHMENT_FOLDER_PATH = 'attachments'
+TOUCH_COMMAND_AVAILABLE = True
+SETFILE_COMMAND_AVAILABLE = False
 
 SYSTEM_DATE_FORMAT = '%m/%d/%Y %H:%M:%S'
 MEM_LINK_PATTERN = r'\(https://mem.ai/m/([A-Za-z0-9]+)\)'
+ATTACHMENT_LINK_PATTERN = r'\!\[\]\((https://storage.googleapis.com/memvp-.*/)([^)]+)\)'
+FILE_EXTENSION_AND_SESSION_PATTERN = r'\.(.+)[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$'
 
 # https://www.anthonysmith.me.uk/2008/01/08/moving-files-to-trash-from-the-mac-command-line/
 def trash_file(file_path):
@@ -23,10 +30,20 @@ def trash_file(file_path):
         print(f"Trashed {file_path}")
 
 def set_file_timestamps(file_path, created, modified):
-    created_command = f'SetFile -d "{created.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
-    call(created_command, shell=True)
-    modified_command = f'SetFile -m "{modified.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
-    call(modified_command, shell=True)
+    created_command = ""
+    if SETFILE_COMMAND_AVAILABLE:
+        created_command = f'SetFile -d "{created.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
+    elif TOUCH_COMMAND_AVAILABLE:
+        created_command = f'touch --date "{created.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
+    if created_command!="":
+        call(created_command, shell=True)
+    modified_command = ""
+    if SETFILE_COMMAND_AVAILABLE:
+        modified_command = f'SetFile -m "{modified.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
+    elif TOUCH_COMMAND_AVAILABLE:
+        modified_command = f'touch -m --date "{modified.strftime(SYSTEM_DATE_FORMAT)}" "{file_path}"'
+    if modified_command!="":
+        call(modified_command, shell=True)
 
 with open(MEM_JSON_PATH) as mems:
     data = json.load(mems)
@@ -59,6 +76,37 @@ with open(MEM_JSON_PATH) as mems:
                 print(f"Converted link to local note: {clean_link}.")
             else:
                 print(f"Warning: https://mem.ai/m/{mem_id} not found in exported notes.")
+
+        # Replace googleapis attachment links with local links where possible.
+        #
+        # In the markdown field, for every occurrence of:
+        #     ![](https://storage.googleapis.com/memvp-...)
+        # download the file, put it in the attachment folder,
+        # and replace the link so that it points to the attachment folder.
+        # The filename of the attachment should have a suitable extension
+        # extracted from the filename.
+        # Example:
+        #  Given: ![](https://storage.googleapis.com/memvp-25499.appspot.com/images/16288199829444671600345520058319.jpga68d4074-e9cc-4f90-b03f-17e82fbb905c)
+        #  Save: 16288199829444671600345520058319.jpga68d4074-e9cc-4f90-b03f-17e82fbb905c
+        #  Replace: ![[16288199829444671600345520058319.jpga68d4074-e9cc-4f90-b03f-17e82fbb905c.jpg]]
+        #
+        for match in re.finditer(ATTACHMENT_LINK_PATTERN, new_note):
+            url_full_match = match.group(0)
+            url_base = match.group(1)
+            url_fname = match.group(2)
+            # Attempt to extract an extension from the filename
+            local_fname = url_fname
+            m=re.search(FILE_EXTENSION_AND_SESSION_PATTERN,url_fname)
+            if m!=None:
+                local_fname = url_fname + "." + m.group(1)
+            try:
+                r = requests.get(url_base+url_fname, allow_redirects=True)
+                with open(ATTACHMENT_FOLDER_PATH+"/"+local_fname, 'wb') as fp:
+                    fp.write(r.content)
+                new_note = new_note.replace(url_full_match, f'![[{local_fname}]]')
+                print(f"Created attachment {local_fname} for remote attachment {url_base+url_fname}")
+            except requests.exceptions.RequestException as e:
+                print(f"Warning: Unable to download and use {url_base+url_fname}. Will NOT convert to local {local_fname} attachment")
 
         with open(writename, "w") as new_file:
             new_file.write(new_note)
